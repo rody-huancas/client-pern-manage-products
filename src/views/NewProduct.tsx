f898936bf7c55008051466e8ced50fc7c95e31c8
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-22 19:59:14 -0500

    Add: design layout

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom"
+
 export const NewProduct = () => {
   return (
-    <div>NewProduct</div>
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">
+          Registrar Producto
+        </h2>
+        <Link to={"/"} className="rounded-md p-3 bg-indigo-600 text-sm font-bold text-white shadow-sm hover:bg-indigo-700">
+          Volver a Productos
+        </Link>
+      </div> 
+    </>
   )
 }
--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom"
+
 export const Products = () => {
   return (
-    <div>Products</div>
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">
+          Productos
+        </h2>
+        <Link to={"/productos/nuevo"} className="rounded-md p-3 bg-indigo-600 text-sm font-bold text-white shadow-sm hover:bg-indigo-700">
+          Agregar Producto
+        </Link>
+      </div> 
+    </>
   )
 }
