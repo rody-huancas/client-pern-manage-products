4a5d33dd4530c06cfcc3188670539d469cefa52e
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-22 21:32:23 -0500

    Add: design form new product

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./layouts/Layout";
 import { Products } from "./views/Products";
-import { NewProduct } from "./views/NewProduct";
+import { NewProduct, action as newProductAction} from "./views/NewProduct";
 
 export const router = createBrowserRouter([
   {
@@ -15,6 +15,7 @@ export const router = createBrowserRouter([
       {
         path: "productos/nuevo",
         element: <NewProduct />,
+        action: newProductAction
       }
     ],
   },
--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -1,4 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link, Form } from "react-router-dom";
+
+export async function action() {
+  console.log("first")
+  return {}
+}
 
 export const NewProduct = () => {
   return (
@@ -7,10 +12,44 @@ export const NewProduct = () => {
         <h2 className="text-4xl font-black text-slate-500">
           Registrar Producto
         </h2>
-        <Link to={"/"} className="rounded-md p-3 bg-indigo-600 text-sm font-bold text-white shadow-sm hover:bg-indigo-700">
+        <Link
+          to={"/"}
+          className="rounded-md p-3 bg-indigo-600 text-sm font-bold text-white shadow-sm hover:bg-indigo-700"
+        >
           Volver a Productos
         </Link>
-      </div> 
+      </div>
+      <Form className="mt-10" method="POST" >
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="name">
+            Nombre Producto:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">
+            Precio:
+          </label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
     </>
-  )
-}
+  );
+};
