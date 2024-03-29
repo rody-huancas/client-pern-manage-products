35113d0225afd447097c54925a19883b70aa7b46
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-29 14:55:07 -0500

    Add: component form field

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,6 +1,7 @@
-import { safeParse } from "valibot";
+import { safeParse, coerce, number, parse } from "valibot";
 import { DraftProductSchema, Product, ProductSchema, ProductsSchema } from "../types";
 import axios from "axios";
+import { toBoolean } from "../utils";
 
 type ProductData = {
   [k: string]: FormDataEntryValue;
@@ -59,5 +60,20 @@ export async function getProductById (id: Product['id']) {
 }
 
 export async function updateProduct (data: ProductData, id: Product['id']) {
+  try {
+    const NumberSchema = coerce(number(), Number);
+    const result = safeParse(ProductSchema, {
+      id: id,
+      name: data.name,
+      price: parse(NumberSchema, data.price),
+      availability: toBoolean(data.availability.toString())
+    });
 
+    if(result.success) {
+      const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+      await axios.put(url, result.output);
+    }
+  } catch (error) {
+    throw error;
+  }
 }
--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -2,6 +2,7 @@ import { Link, Form, useActionData, LoaderFunctionArgs, redirect, useLoaderData,
 import { ErrorMessage } from "../components/ErrorMessage";
 import { getProductById, updateProduct } from "../services/ProductService";
 import { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 
 export async function loader( { params }: LoaderFunctionArgs ) {
   if (params.id !== undefined) {
@@ -55,32 +56,8 @@ export const EditProduct = () => {
         error && <ErrorMessage>{error}</ErrorMessage>
       }
       <Form className="mt-10" method="POST" >
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="name">
-            Nombre Producto:
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-            defaultValue={product.name}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="price">
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-            defaultValue={product.price}
-          />
-        </div>
+
+        <ProductForm product={product} />
 
         <div className="mb-4">
           <label
@@ -101,7 +78,7 @@ export const EditProduct = () => {
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-          value="Registrar Producto"
+          value="Guardar Cambios"
         />
       </Form>
     </>
