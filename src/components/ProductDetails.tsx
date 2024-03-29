fa1953c40395cc55fd6537ef30e30a375452dabd
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-29 13:16:56 -0500

    Add: form edit

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Product } from "../types/index";
 import { formatCurrency } from "../utils";
 
@@ -6,15 +7,26 @@ type ProductDetailProps = {
 };
 
 const ProductDetails = ({ product }: ProductDetailProps) => {
-
-    const isAvailable = product.availability;
-
+  
+  const navigate = useNavigate();
+  const isAvailable = product.availability;
+  
   return (
     <tr className="border-b ">
       <td className="p-3 text-lg text-gray-800">{product.name}</td>
       <td className="p-3 text-lg text-gray-800">{formatCurrency(product.price)}</td>
       <td className="p-3 text-lg text-gray-800">{isAvailable ? "Disponible" : "No Disponible"}</td>
-      <td className="p-3 text-lg text-gray-800"></td>
+      <td className="p-3 text-lg text-gray-800">
+        <div className="flex gap-2 items-center">
+          <button onClick={() => navigate(`/productos/${product.id}/editar`, {
+            state: {
+              product
+            }
+          })}
+            className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs"
+          >Editar</button>
+        </div>
+      </td>
     </tr>
   );
 };
